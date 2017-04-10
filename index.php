ffcafeae5ee7fd036b88a453cac7ceaedfb9aa0b
avinash1304 <avinashprasad1304@gmail.com>
2017-04-10 23:37:55 +0530

    version 1.1

--- index.php
+++ index.php
@@ -1,273 +1,58 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> 
-  <![endif]-->
-<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang="en"> 
-  <![endif]-->
-  <!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
-  <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-  <head>
-  	<!--tITLE sTATRT-->
-    <title>Einfach Solutions</title>
-    <meta charset="UTF-8">
-    <meta name="description" content="Einfach means 'Simple' and that is exactly what we do. We simplify your issues and provide solutions that rightly term us as a next generation solution provider. We are not just another Business Process Outsources. Our aim is to provide our clients with solutions that will help them gain control and efficiencies in the ever changing service industry.">
-    <meta name="keywords" content="einfach,outsourcing,offshore,mortgage,technology,consulting">
-	<meta name="author" content="Avinash Prasad">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php require_once("resources/config.php"); ?>
+
+<?php 
+		
+	$pagetitle = 'Mortgage Services, Title &amp; Settlement, Vendor Management, Out/In Bound Call, Web Development, Digital Marketing';	
+
+	include(TEMPLATE_FRONT . DS . "header.php"); 
+
+?>
+
+<?php include(TEMPLATE_FRONT . DS . "slider.php"); ?>
+
+
+
+
+ <!-- Call To Action -->
+	<div id="cta">
+		<div class="container centered">
+			<h3>Welcome to the Einfach World!</h3>
+			<p>If you want to reduce costs &amp; want to get more time for your people to focus on strategic thinking, <strong>“ES”</strong> is a real answer.</p>
+			<br>
+			<a href="about.php"><button class="btn btn-embossed btn-primary">About Us</button></a>
+		</div><!-- /container -->
+	</div><!-- /cta -->
 	
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-    <link href='http://fonts.googleapis.com/css?family=Comfortaa:400,700' rel='stylesheet' type='text/css'>
-
-    <!-- CSS Bootstrap & Custom -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">  
-    <link href="slider/flexslider.css" rel="stylesheet" type="text/css" media="screen" />
-    <link href="css/templatemo_style.css" rel="stylesheet" type="text/css">
-
-    <!-- Modernizr -->
-    <script src="slider/modernizr.js"></script>
-    <!-- HTML 5 shim for IE backwards compatibility -->
-  <!-- [if lt IE 9]>
-  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
-  </script>
-  <![endif]-->
-</head>
-<body>
-
-  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">    
-    <div class="container">
-      <div class="row">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="col-md-2 col-sm-2 col-xs-12">
-          <div class="navbar-header"> 
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a href="index.php" class="navbar-brand"><img src="images/logo.png" alt="Einfach Solutions" width="85px" height="75px" style="margin-top:-10px;"></a>
-          </div>
-        </div>        
-        
-        <div class="col-md-10 col-sm-10 col-xs-12">
-          <div class="navbar-collapse collapse menu">          
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#home"><i class="fa fa-home"></i>Home</a></li>
-              <li><a href="#about"><i class="fa fa-users"></i>About Us</a></li>
-              <li><a href="#blog"><i class="fa fa-bookmark"></i>Careers</a></li>
-              <li><a href="#services"><i class="fa fa-cogs"></i>Services</a></li>
-              <li><a href="#contact"><i class="fa fa-envelope"></i>Contact</a></li>
-            </ul>
-          </div><!-- /.navbar-collapse -->
-        </div>
-      </div><!-- /.row -->
-    </div>
-  </nav>
-
-  <div class="container" id="home">
-    <div class="row">
-      <div class="col col-md-12">        
-        <div class="flexslider">
-          <ul class="slides">
-            <li>
-              <img src="images/slide1.jpg" alt="slide 1" />
-              <p class="flex-caption" align="center">"If you want to reduce costs & want to get more time for your people to focus on strategic thinking, “ES” is a real answer."</p>
-            </li>
-            <li>
-              <img src="images/slide2.jpg" alt="slide 2" />
-              <p class="flex-caption" align="center">"You want it, We have it"</p>
-            </li>
-            <li>
-              <img src="images/slide3.jpg" alt="slide 3" />
-              <p class="flex-caption" align="center">Technology is the collection of tools, including machinery, modifications, arrangements and procedures used by humans.</p>
-            </li>
-          
-          </ul>
-        </div>            
-      </div>
-    </div>
-  </div>
-<!--about page-->  
-  <section id="about">
-    <div class="outer_container">
-      <div class="container">
-        <div class="row">
-          <div class="col col-md-5 col-sm-12" style="margin-top: 7.5%;"><h1>About us</h1></div>
-            <div class="col col-md-7 col-sm-12 center-row" id="about_content">
-              <div class="row center">
-                <div class="col col-md-8 col-sm-12" align="justify" style="width:100%;">
-              	<p>Einfach means "Simple" and that is exactly what we do. We simplify your issues and provide solutions that rightly term us as a next generation solution provider. We are not just another Business Process Outsources. Our aim is to provide our clients with solutions that will help them gain control and efficiencies in the ever changing service industry.</p>
-                <p>We are a bunch extremely passionate about business with over 35 years of combined experience in setting up new businesses, transition and Business process improvements.</p>
-                <p>Our fully integrated platform has been designed for clients who are seeking a strategic partner to deliver measurable outcomes. It has been architected to provide all of the capabilities that an organization needs to create a differentiated customer experience across all channels and all phases of the customer life cycle. It balances the latest innovations in technology with the empathy of human contact which allows our clients to focus on their core competencies while we focus on ours - delivering awesome customer experiences.</p>
-                <p>We understand the uniqueness of every transaction and give it the attention it deserves. This approach has helped us and our clients to complete transactions seamlessly. We offer customized solutions based on our client’s requirements.</p>
-                <p>With an over all 35 years of real estate, outsourcing, technology and consulting experience, our team has seen the ups and downs of the outsourcing business,our market understanding and approach to solution design is state of the art.</p>
-               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-</div>
-
-<!--Careers Page-->
-  <section id="blog">
-    <div class="outer_container">
-      <div class="container">
-        <div class="row">
-          <div class="col col-md-5 col-md-push-7 col-sm-12" style="margin-top: 5%;"><h1>Careers</h1></div>
-          <div class="col col-md-7 col-md-pull-5 col-sm-12" align="justify">
-            <div class="blog_posts_container">
-              <p><strong>We are currently hiring!</strong></p>
-              <div>
-                <strong>Title Searchers and Examiners</strong> 
-                <ul type="disc"><li>Candidates with at least 1 year experience in Title Search and 2 years’ experience in Title Examination.</li></ul>
-              </div>    
-              <div>
-                <strong>Account Manager</strong>      
-                <ul type="disc"><li>Candidates with excellent telephone etiquette, customer handling skills and with experience handling phone calls at an International Call Centre. (Night Shift)</ul>
-              </div>
-              <div>
-                <strong>Web Designer/Developer</strong>      
-                <ul type="disc"><li>Candidates with experience in Web Design and Development, the ideal candidate would be an expert in PHP and would have experience working for a start-up.</ul>
-              </div>
-              <div>
-                <strong>Graphic Designer</strong>      
-                <ul type="disc"><li>Candidates with experience in creating logos, brochures, with very high level of creativity</li></ul>
-              </div><br/>
-              <p align="justify">Should you be interested in any of these positions, please send your resume, with details of your current salary, expected salary and notice period and we will give you a call.</p>
-              <p align="justify">Or you may walk-in to our office any time after 5 PM on Weekdays for a personal interaction.</p>
-            </div>          
-          </div>              
-        </div>
-      </div>
-    </div>
-  </section>
-  <section id="services">
-    <div class="outer_container">
-      <div class="container">
-        <div class="row">
-          <div class="col col-md-5 col-sm-12 col-xs-12">
-            <h1>Services</h1>
-          </div>
-          <div class="col-md-2 col-sm-2 col-xs-2">
-            <div class="services_buttons">
-              <ul id="services_tabs">
-                <!--<li class="icon-button active" data-target="#pencil"><i class="fa fa-group" style="vertical-align: middle"></i></li>-->
-                <li class="icon-button active" data-target="#cog"><i class="fa fa-money"></i></li>            
-                <li class="icon-button" data-target="#signal"><i class="fa fa-headphones"></i></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col col-md-5 col-sm-10 col-xs-10 center-row" id="services_content">
-            <div class="arrow-left"></div>
-            <div class="arrow-box center-row">
-              <div class="tab-content center">              
-                <!--<div class="tab-pane fade in active" id="pencil">
-                  	<h3>MANAGEMENT</h3>
-                  	<p align="justify">With an over all 35 years of real estate, outsourcing, technology and consulting experience, our team has seen the ups and downs of the outsourcing business,our market understanding and approach to solution design is state of the art.</p>
-                </div>-->
-                <div class="tab-pane fade in active" id="cog">
-                	<h3>MORTGAGE SERVICES</h3>
-                  	<p><strong>Mortgage Processing</strong></p>
-                  	<p><strong>Title Services</strong></p>
-                    <p><strong>Closing/Signing Services</strong></p>
-                    <p><strong>Recording Services</strong></p>
-                    <p><strong>Vendor Management</strong></p>
-                </div>
-                <div class="tab-pane fade" id="signal">
-                	<h3>BPO SERVICES</h3>
-                  	<p><strong>Out Bound Call Services</strong></p>
-                    <p><strong>In Bound Call Services</strong></p>
-                    <p><strong>Healthcare Recruiting Services</strong></p>
-                </div>
-              </div>                                     
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-    <section id="contact">
-    <div class="outer_container">
-      <div class="container">
-        <div class="row">
-          <div class="col col-md-5 col-md-push-7 col-sm-12"><h1>Map</h1></div>
-          <div class="col col-md-7 col-md-pull-5 col-sm-12">
-            <div id="google_map">
-            </div>
-          </div>              
-        </div>
-      </div>
-    </div>
-  </section>
-  <footer>
-    <div class="container">        
-      <div class="row">
-        <div class="col col-md-6 col-sm-6 col-xs-6 col-xxs">
-          <p style="margin-top:2%;" align="justify">
-                <strong>Einfach Solutions</strong><br/>
-                764/1, 3rd Floor, Vishal Towers
-                19th Main, Sector 2,
-                HSR Layout, Bangalore - 560102<br/>
-                Phone - +91 806 555 5868
-                Email – Info@einfachltd.com</p>
-          <div id="templatemo_copyright">
-            Copyright © <?php echo date('Y')?> <a href="index.php">Einfach Solutions</a>
-          </div>
-        </div>        
-        <div class="col col-md-6 col-sm-6 col-xs-6 col-xxs">
-          <div class="pull-right">
-            <div class="social_buttons">
-              <a href="https://www.facebook.com/pages/Einfach-Solutions/680324415328122" rel="nofollow" class="icon-button facebook"><i class="fa fa-facebook"></i></a>
-              <a href="https://twitter.com/EinfachInfo" rel="nofollow" class="icon-button twitter"><i class="fa fa-twitter"></i></a>             
-              <a href="https://www.linkedin.com/company/einfach-solutions" rel="nofollow" class="icon-button linkedin"><i class="fa fa-linkedin"></i></a>
-            </div>
-            <a href="#" id="go-top"><div class="back_to_top">Back To Top</div></a>
-          </div>            
-        </div>
-      </div> 
-    </div>
-  </footer>
-
-  <!-- JavaScripts -->
-  <script src="js/jquery.min.js"></script>
-  <script src="bootstrap/js/bootstrap.min.js"></script>
-  <script src="js/jquery.singlePageNav.js"></script>
-  <script src="js/templatemo_custom.js"></script>
-  <script defer src="slider/jquery.flexslider.js"></script>
-
-  <script type="text/javascript">
-
-    $(window).load(function(){
-      $('.flexslider').flexslider({
-        animation: "slide",
-        start: function(slider){
-          $('body').removeClass('loading');
-          $('.menu').singlePageNav();
-        }
-      });
-    });
-
-    $('#go-top').click(function(event) {
-      event.preventDefault();
-      jQuery('html, body').animate({scrollTop: 0}, 1000);
-      return false;
-    });
-
-  </script>
-  
-    <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-42219727-2', 'auto');
-  ga('send', 'pageview');
-
-  </script>
-  <!-- Avinash Prasad -->
-</body>
-</html>
-
+	
+	<!-- Services Section -->
+	<div class="container sinfo">
+		<div class="row svgicons centered">
+		
+			<div class="col-lg-4">
+				<img src="assets/images/icons/os.svg">
+				<h4><b>Outsourcing Services</b></h4>
+				
+			</div><!-- col-lg-4 -->
+			
+			<div class="col-lg-4">
+				<img src="assets/images/icons/dms.svg">
+				<h4><b>Marketing &amp; Advertising </b></h4>
+				
+			</div><!-- col-lg-4 -->
+			
+			<div class="col-lg-4">
+				<img src="assets/images/icons/wds.svg">
+				<h4> <b>Web Development</b></h4>
+				
+			</div><!-- col-lg-4 -->
+		
+		</div><!-- /row -->
+		<br>
+		
+	</div><!-- /container -->
+	
+	
+	<!-- Portfolio Section -->
+	
+	
+<?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
